01aa0eaec1569990df4620345626dc4369c10da6
nikhilsinha822 <113183657+nikhilsinha822@users.noreply.github.com>
2023-01-03 00:48:08 +0530

    navbar update

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import logoBlue from '../../assets/logoBlue.svg'
 import './navbar.css';
 
 function Navbar() {
@@ -12,19 +13,31 @@ function Navbar() {
   return (
     <header>
       <nav ref={navRef}>
-        <a onClick={showNavbar} href="#About">About</a>
-        <a onClick={showNavbar} href="/">Events</a>
-        <a onClick={showNavbar} href="#Domains">Domains</a>
-        <a onClick={showNavbar} href="/Team">Team</a>
-        <a onClick={showNavbar} href="#Contact">Contact Us</a>
+        
+        <span className="logo"><img src={logoBlue} alt="logo"/><span className="logo-name">BDCOE</span></span>
+        
+        <span className="links">
+          <a onClick={showNavbar} href="#About">About</a>
+          <a onClick={showNavbar} href="/">Events</a>
+          <a onClick={showNavbar} href="#Domains">Domains</a>
+          <a onClick={showNavbar} href="/Team">Team</a>
+          <a onClick={showNavbar} href="#Contact">Contact Us</a>
+        </span>
+
+        
         <button class="regbtn">Register</button>
+        
         <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-          <FaTimes />
+        <FaTimes />
+          
         </button>
       </nav>
-      <button className="nav-btn nav-open-btn" onClick={showNavbar}>
-        <FaBars />
+      <div className="resnav"><button className="nav-btn nav-open-btn" onClick={showNavbar}>
+      <FaBars />
       </button>
+      </div>
+      
+        
     </header>
   );
 }
