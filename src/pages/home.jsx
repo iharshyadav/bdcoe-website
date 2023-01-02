ede56cc26cafdb96319cb9a7b960e606fee45731
nikhilsinha822 <113183657+nikhilsinha822@users.noreply.github.com>
2023-01-02 19:50:19 +0530

    added footer

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import About from "../components/about/about.jsx"
 import ContactUs from "../components/contactUs/contactUs.jsx";
+import Footer from "../components/footer/footer.jsx";
 const Home = () =>{
     return(
         <>
             <About/>
             <ContactUs/>
+            <Footer/>
         </>
     )
 }
