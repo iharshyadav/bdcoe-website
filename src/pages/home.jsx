ab46b27aa7bb9d4f1e91897a71a665310130c0d4
nikhilsinha822 <113183657+nikhilsinha822@users.noreply.github.com>
2023-01-02 19:44:37 +0530

    contact us added

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import About from "../components/about/about.jsx"
+import ContactUs from "../components/contactUs/contactUs.jsx";
 const Home = () =>{
     return(
         <>
             <About/>
+            <ContactUs/>
         </>
     )
 }
