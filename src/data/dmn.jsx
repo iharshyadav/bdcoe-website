8f74fd3f50cccf939cd2b0fd81d17d7b5fe43abc
nikhilsinha822 <113183657+nikhilsinha822@users.noreply.github.com>
2023-01-03 18:04:16 +0530

    content update

--- src/data/dmn.jsx
+++ src/data/dmn.jsx
@@ -9,37 +9,37 @@ export const Domain=[
         id: 1,
         image: ML,
         name: "Machine Learning",
-        content: "as programming and database management, as well as design skills, such as user experience (UX) design and graphic design. Developers may work on a team with other professionals, such as project managers, designers, and quality assurance testers, to complete the app development process.",
+        content: "Machine learning is a method of data analysis that automates analytical model building. It is a branch of artificial intelligence that involves training algorithms to learn from and make predictions on data.",
     },
     {
         id: 2,
         image: CP,
         name: "Competitive Coding",
-        content: "as programming and database management, as well as design skills, such as user experience (UX) design and graphic design. Developers may work on a team with other professionals, such as project managers, designers, and quality assurance testers, to complete the app development process.",
+        content: "Competitive programming is a type of activity in which individuals solve coding problems and challenges under time pressure to improve their programming skills and prepare for technical interviews. It requires a strong understanding of algorithms and data structures, and is often a competitive activity.",
     },
     {
         id: 3,
         image: AD,
         name: "App Development",
-        content: "as programming and database management, as well as design skills, such as user experience (UX) design and graphic design. Developers may work on a team with other professionals, such as project managers, designers, and quality assurance testers, to complete the app development process.",
+        content: "App development is the process of creating software applications for mobile devices, such as smartphones and tablets. It involves designing, coding, testing, and maintaining apps to ensure that they function correctly and meet user needs.",
     },
     {
         id: 4,
         image: WD,
         name: "Web Development",
-        content: "as programming and database management, as well as design skills, such as user experience (UX) design and graphic design. Developers may work on a team with other professionals, such as project managers, designers, and quality assurance testers, to complete the app development process.",
+        content: "Web development is the process of building and maintaining websites. It involves creating the layout and design of a site, writing code to implement the desired features and functionality, and ensuring that the site is optimized for search engines and mobile devices.",
     },
     {
         id: 5,
         image: BD,
         name: "Big Data",
-        content: "as programming and database management, as well as design skills, such as user experience (UX) design and graphic design. Developers may work on a team with other professionals, such as project managers, designers, and quality assurance testers, to complete the app development process.",
+        content: "Big data is a term used to describe extremely large data sets that can be analyzed to reveal patterns, trends, and associations. It is used to inform decision making and improve efficiency in fields such as business, science, and government.",
         
     },
     {
         id: 6,
         image: DSGN,
         name: "Designing",
-        content: "as programming and database management, as well as design skills, such as user experience (UX) design and graphic design. Developers may work on a team with other professionals, such as project managers, designers, and quality assurance testers, to complete the app development process.",
+        content: "Designing refers to the process of creating a plan or blueprint for something, such as a product, system, or service. It involves identifying the needs and goals of the project, and creating a solution that meets those needs and goals while considering factors such as aesthetics, functionality, and cost.",
     },
 ]
