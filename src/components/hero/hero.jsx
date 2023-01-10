e7542b7c32acfebcf0a0546a0a0519155d496b1c
nikhilsinha822 <113183657+nikhilsinha822@users.noreply.github.com>
2023-01-10 19:15:34 +0530

    hero page svg added

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,8 +1,13 @@
  import './hero.css'
+ import Icon from './server'
 
  const Hero = () =>{
-    return <div className="home" id="home">
+    return <div className="home grid-container" id="home">
     {/* <div class="box1"></div> */}
+    <div className='grid-item img-grid'>
+        <Icon/>
+    </div>
+    <div className='grid-item'>
         <div className="t1">    
             <h1>BIG DATA - CENTER OF EXCELLENCE</h1>
         </div>
@@ -12,6 +17,7 @@
         <div className="t3">          
             <h3>We Learn | We Develop | We Execute</h3>
         </div>
+    </div>
     </div>
  }
 
