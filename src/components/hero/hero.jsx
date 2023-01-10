9f52b2ba6ce31955c27273a093ff6c84cd6b785a
nikhilsinha822 <113183657+nikhilsinha822@users.noreply.github.com>
2023-01-10 22:54:17 +0530

    font update

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,20 +1,19 @@
  import './hero.css'
  import Icon from './server'
-
  const Hero = () =>{
     return <div className="home grid-container" id="home">
     {/* <div class="box1"></div> */}
-    <div className='grid-item img-grid'>
+    <div className='grid-item img-grid '>
         <Icon/>
     </div>
-    <div className='grid-item'>
+    <div className='grid-item hidden'>
         <div className="t1">    
             <h1>BIG DATA - CENTER OF EXCELLENCE</h1>
         </div>
-        <div className="t2">    
+        <div className="t2 hidden">    
             <h2>Decipher Your Destiny</h2>
         </div>
-        <div className="t3">          
+        <div className="t3 hidden">          
             <h3>We Learn | We Develop | We Execute</h3>
         </div>
     </div>
