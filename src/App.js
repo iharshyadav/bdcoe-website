58cac8253813275a11ebe949dbf6b748f9bec225
nikhilsinha822 <113183657+nikhilsinha822@users.noreply.github.com>
2023-01-17 03:49:16 +0530

    routes fixed

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import Home from './pages/home'
 import Event from './pages/events'
 import Team from './pages/team'
 import './App.css';
-import {Routes ,Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes ,Route} from 'react-router-dom'
 
 const App = () => {
 	return (
 		<>
-		<Routes>
-		<Route path='/' element={<Home/>}/>		
-		<Route path='/team' element={<Team/>} />
-		<Route path='/event' element={<Event/>} />		
-		</Routes>
+		<Router>
+			<Routes>
+				<Route exact path='/' element={<Home/>}/>		
+				<Route path='/team' element={<Team/>} />
+				<Route path='/event' element={<Event/>} />		
+			</Routes>
+		</Router>
 		</>
 	);
 }
