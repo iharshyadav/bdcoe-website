5b9b57b407c4048d4bbc2c9869af6e8fb6d5b62f
Prateek Chaurasia <prateek18801@gmail.com>
2023-01-17 15:17:10 +0530

    update content restructure components

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,55 +1,51 @@
 import React from 'react'
 import logoWhite from '../../assets/logo-white1.svg'
-import './footer.css'  
+import './footer.css'
 
 const Footer = () => {
-  return (
-    <>
-    <div className='footer'>
-        <div className='footer-container'>
-            <div className="left footer-child">
-                <div className="content">
-                <h1><span> Big Data </span> - Centre Of Excellence</h1>
-                <br/>
-                <p className='cnt'>    
-                is the R & D Centre established under Ajay Kumar Garg Engineering College, Ghaziabad. <br/>
-                
-                At BDCE , we strive to stimulate interest in Big Data concept and related technologies among the students of the Instutions. Besides making serious endeavors to spread knowledge on Big Data, we stand by the stuble acronym of TEAM - 'Together Everyone Achieves More'.
-                </p>
+    return (
+        <>
+            <div className='footer'>
+                <div className='footer-container'>
+                    <div className="left footer-child">
+                        <div className="content">
+                            <h1><span> Big Data </span> - Centre Of Excellence</h1>
+                            <br />
+                            <p className='cnt'>
+                            At Big Data Centre of Excellence, we believe that collaboration and teamwork are key to success. Our mission is to help students gain knowledge on Big Data technologies and concepts to leverage their potential and achieve success in their respective fields. Besides making serious endeavors to spread knowledge on Big Data, we stand by the stuble acronym of TEAM - "Together Everyone Achieves More".
+                            </p>
+                        </div>
+                        <div className="menu">
+                            <ul>
+                                <li><a href="/">Home</a></li>
+                                <li><a href="#Domains">Domains</a></li>
+                                <li><a href="/team">Team</a></li>
+                                <li><a href="#About">About</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className='footer-child logo-child'>
+                        <div className='footer-logo'>
+                            <img src={logoWhite} alt="logo" />
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className='footer-child logo-child'>
-                <div className='footer-logo'>
-                    <img src={logoWhite} alt="logo"/>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div className="copyright">
-            <div className='menu'>
+
+            <div className="social">
                 <ul>
-                    <a href="/"><li>Home</li></a>
-                    <li className='sep'>|</li>
-                    <a href="#Domains"><li>Domains</li></a>
-                    <li className='sep'>|</li>
-                    <a href="/team"><li>Team</li></a>
-                    <li className='sep'>|</li>
-                    <a href="#About"><li>About</li></a>
+                    <li><a href="mailto:bdcoe@akgec.ac.in" target="_blank" rel="noreferrer"><i className="fa-brands fa-google"></i></a></li>
+                    <li><a href="https://www.linkedin.com/school/big-data-centre-of-excellence/" target="_blank" rel="noreferrer"><i className="fa-brands fa-linkedin"></i></a></li>
+                    <li><a href="https://www.youtube.com/@bigdatacentreofexcellence3334" target="_blank" rel="noreferrer"><i className="fa-brands fa-youtube"></i></a></li>
+                    <li><a href="https://www.instagram.com/bdcoe/" target="_blank" rel="noreferrer"><i className="fa-brands fa-square-instagram"></i></a></li>
+                    <li><a href="https://github.com/Big-Data-Center-of-Excellence" target="_blank" rel="noreferrer"><i className="fa-brands fa-github"></i></a></li>
                 </ul>
             </div>
-           <div className="social">  
-                <ul>
-                    <li><a href="mailto:bdcoe@akgec.ac.in" target="_blank" rel="noreferrer"><i class="fa-brands fa-google"></i></a></li>
-                    <li><a href="https://www.linkedin.com/school/big-data-centre-of-excellence/" target="_blank" rel="noreferrer"><i class="fa-brands fa-linkedin"></i></a></li>
-                    <li><a href="https://www.youtube.com/@bigdatacentreofexcellence3334" target="_blank" rel="noreferrer"><i class="fa-brands fa-youtube"></i></a></li>
-                    <li><a href="https://www.instagram.com/bdcoe/" target="_blank" rel="noreferrer"><i class="fa-brands fa-square-instagram"></i></a></li>
-                    <li><a href="https://github.com/Big-Data-Center-of-Excellence" target="_blank" rel="noreferrer"><i class="fa-brands fa-github"></i></a></li>
-                </ul>
+            <div className="copyright">
+                Copyright ©️ {new Date().getFullYear()} - Big Data Centre of Excellence
             </div>
-        Copyright ©️ {new Date().getFullYear()} - Big Data Centre of Excellence
-    </div>
-    </> 
-  )
+        </>
+    )
 }
 
 export default Footer
