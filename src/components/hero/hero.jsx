3cea8fe62ae63e55899d2624d73dfafa4592887b
Prateek Chaurasia <prateek18801@gmail.com>
2023-01-18 02:42:09 +0530

    update animated graphic remove static

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,33 +1,32 @@
- import React,{useEffect} from "react"
- import './hero.css'
- import Icon from './server'
- import Aos from "aos";
- import "aos/dist/aos.css"
+import React, { useEffect } from "react"
+import './hero.css'
+import Graphic from './graphic'
+import Aos from "aos";
+import "aos/dist/aos.css"
 
- const Hero = () =>{
-    useEffect(()=>{
-        Aos.init({duration : 2000})
-      }, []);
+const Hero = () => {
+    useEffect(() => {
+        Aos.init({ duration: 2000 })
+    }, []);
     return <div className="home grid-container" id="home">
-    {/* <div class="box1"></div> */}
-    <div className='grid-item img-grid'>
-        <Icon/>
-    </div>
-    <div className='grid-item hidden' 
-    // data-aos="flip-left"
-    >
-        <div className="t1">    
-            <h1>BIG DATA </h1> 
-            <h1>CENTER OF EXCELLENCE</h1>
+        <div className='grid-item img-grid'>
+            <Graphic/>
         </div>
-        <div className="t2 hidden">    
-            <h2>Decipher Your Destiny</h2>
-        </div>
-        <div className="t3 hidden">          
-            <h3>We Learn | We Develop | We Execute</h3>
+        <div className='grid-item hidden'
+        // data-aos="flip-left"
+        >
+            <div className="t1">
+                <h1>BIG DATA </h1>
+                <h1>CENTRE OF EXCELLENCE</h1>
+            </div>
+            <div className="t2 hidden">
+                <h2>Decipher Your Destiny</h2>
+            </div>
+            <div className="t3 hidden">
+                <h3>We Learn | We Develop | We Execute</h3>
+            </div>
         </div>
     </div>
-    </div>
- }
+}
 
- export default Hero;
+export default Hero;
