31abb0f6460fc10e92dfeba24454ddff9e5d826d
Prateek Chaurasia <prateek18801@gmail.com>
2023-01-18 03:44:23 +0530

    minor content changes

--- src/components/contactUs/contactUs.jsx
+++ src/components/contactUs/contactUs.jsx
@@ -1,106 +1,100 @@
-import React, {useState,useRef} from "react";
+import React, { useState, useRef } from "react";
 import "./contactUs.css";
 import ReCAPTCHA from "react-google-recaptcha"
 import axios from "axios";
 
 const ContactUs = () => {
   const [verified, setVerified] = useState(false);
-  const [recaptchaValue, setRecaptchaValue]= useState();
-  const captchaRef =useRef();
+  const [recaptchaValue, setRecaptchaValue] = useState();
+  const captchaRef = useRef();
 
   const [user, setUser] = useState({
-    name:"",
-    email:"",
-    message:"",
+    name: "",
+    email: "",
+    message: "",
   });
 
-  const SITE_KEY="6Ld0ftEjAAAAAOodkf282VwH7MMXFat3qRstfXQx"
-  const TEST_KEY="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+  const SITE_KEY = "6Ld0ftEjAAAAAOodkf282VwH7MMXFat3qRstfXQx"
+  const TEST_KEY = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
 
-  const handleEvent =(e) =>{
-      const { name , value }=e.target
-      setUser({
-        ...user,
-        [name] : value
-      })
-      //console.log(user)
+  const handleEvent = (e) => {
+    const { name, value } = e.target
+    setUser({
+      ...user,
+      [name]: value
+    })
+    //console.log(user)
   }
 
-  const handleSubmit =(e)=>{
-      e.preventDefault();
-      captchaRef.current.reset();
-      axios.post('https://bdcoe.onrender.com/api/v1/contact',{
-        ...user,
-        recaptchaValue
-      }).then((res)=>{alert(res.data.message)})
-      .catch(error=>{alert(error.response.message)});
-      //console.log({...user,recaptchaValue})
-      setUser({
-        name:"",
-        email:"",
-        message:"",
-      })
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    captchaRef.current.reset();
+    axios.post('https://bdcoe.onrender.com/api/v1/contact', {
+      ...user,
+      recaptchaValue
+    }).then((res) => { alert(res.data.message) })
+      .catch(error => { alert(error.response.message) });
+    setUser({
+      name: "",
+      email: "",
+      message: "",
+    })
   }
   return (
-    <div id="Contact">
+    <div id="contact">
       <div className="contact">
         <div className="con-heading">
           <h1>Contact Us</h1>
         </div>
         <div className="con-body">
           <div className="con-left">
-            {/* <h2>+91XXXXXXXXXX</h2> */}
-            <h3>www.bdcoe.co.in</h3>
-            <h3>If you have any queries, drop it here.</h3>
-             
+            <h3>If you have any queries, drop them here :</h3>
           </div>
-          
+
           <div className="contact-container">
             <div className="form">
               <form onSubmit={handleSubmit}>
                 <span className="scndclm">
                   <div className="">
                     <label className="lbl">Name</label>
-                    <input 
-                    type="text" 
-                    name="name" 
-                    placeholder="Ram"
-                    onChange={handleEvent} 
-                    value={user.name}
-                    required />
+                    <input
+                      type="text"
+                      name="name"
+                      placeholder="Full Name"
+                      onChange={handleEvent}
+                      value={user.name}
+                      required />
                     <label className="lbl">Email</label>
-                    <input 
-                    type="email" 
-                    name="email" 
-                    placeholder="xyz@gmail.com" 
-                    onChange={handleEvent}
-                    value={user.email}
-                    required/>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="Email Address"
+                      onChange={handleEvent}
+                      value={user.email}
+                      required />
                   </div>
                   <div>
                     <label className="lbl">Message</label>
-                    <textarea 
-                    name="message" 
-                    placeholder="Your Message Here" 
-                    onChange={handleEvent}
-                    value={user.message}
-                    required/>
+                    <textarea
+                      name="message"
+                      placeholder="Your Message Here"
+                      onChange={handleEvent}
+                      value={user.message}
+                      required />
                   </div>
                 </span>
                 <ReCAPTCHA
-                      sitekey={SITE_KEY}
-                      onChange={(value) => {
-                        setVerified(!verified);
-                        setRecaptchaValue(value);
-                        
-                        //console.log(value);
-                      }}
-                      ref={captchaRef}
-                      className="captcha"
+                  sitekey={SITE_KEY}
+                  onChange={(value) => {
+                    setVerified(!verified);
+                    setRecaptchaValue(value);
+                  }}
+                  ref={captchaRef}
+                  className="captcha"
                 />
                 <div>
-                  <input className="btn" type="submit" value="Send" disabled={!verified}/>
-                </div>  
+                  <input className="btn" type="submit" value="Send" disabled={!verified} />
+                </div>
               </form>
             </div>
             <div className="map">
