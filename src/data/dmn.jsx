535c06a445c674537219e4c5176195a3006ae430
Prateek Chaurasia <prateek18801@gmail.com>
2023-01-17 14:18:51 +0530

    update reorder domain card content

--- src/data/dmn.jsx
+++ src/data/dmn.jsx
@@ -7,39 +7,39 @@ import DSGN from '../assets/images/DSGN.svg';
 export const Domain=[
     {
         id: 1,
-        image: ML,
-        name: "Machine Learning",
-        content: "Machine learning is a method of data analysis that automates analytical model building. It is a branch of artificial intelligence that involves training algorithms to learn from and make predictions on data.",
+        image: BD,
+        name: "Big Data",
+        content: "Big data is a term used to describe large and complex datasets that cannot be handled by traditional data processing applications. It is the collection of massive amounts of structured and unstructured data from different sources that can be used to identify patterns, trends and correlations to help companies make better decisions and gain a competitive advantage.",
+        
     },
     {
         id: 2,
-        image: CP,
-        name: "Competitive Coding",
-        content: "Competitive programming is a type of activity in which individuals solve coding problems and challenges under time pressure to improve their programming skills and prepare for technical interviews. It requires a strong understanding of algorithms and data structures, and is often a competitive activity.",
+        image: ML,
+        name: "Machine Learning",
+        content: "Machine learning is the process of using algorithms to enable systems to learn and improve from experience without being explicitly programmed. It is a subset of artificial intelligence that focuses on computer algorithms to learn from data, identify patterns, and make decisions. It is a powerful tool for businesses as it can help to make accurate predictions and increase efficiency by automating multiple tasks.",
     },
     {
         id: 3,
-        image: AD,
-        name: "App Development",
-        content: "App development is the process of creating software applications for mobile devices, such as smartphones and tablets. It involves designing, coding, testing, and maintaining apps to ensure that they function correctly and meet user needs.",
+        image: CP,
+        name: "Competitive Coding",
+        content: "Competitive programming is a mind sport where participants attempt to write programs to solve a given problem faster and more accurately than others. Competitive programming is a great way to hone programming and problem solving skills, as well as to learn about algorithms and data structures. Additionally, it is a great way to practice coding and develop logical thinking skills.",
     },
     {
         id: 4,
-        image: WD,
-        name: "Web Development",
-        content: "Web development is the process of building and maintaining websites. It involves creating the layout and design of a site, writing code to implement the desired features and functionality, and ensuring that the site is optimized for search engines and mobile devices.",
+        image: AD,
+        name: "App Development",
+        content: "App development is an emerging field due to the ever increasing popularity of mobile devices and the demand for more powerful and feature rich applications. It requires a deep understanding of the mobile platform, user interface design, and the capabilities of the device. App development is a rewarding and challenging career, and those with the right skillset have the potential to create highly successful and popular apps.",
     },
     {
         id: 5,
-        image: BD,
-        name: "Big Data",
-        content: "Big data is a term used to describe extremely large data sets that can be analyzed to reveal patterns, trends, and associations. It is used to inform decision making and improve efficiency in fields such as business, science, and government.",
-        
+        image: WD,
+        name: "Web Development",
+        content: "Web development is the process of building, creating, and maintaining websites. It involves web design, web content development, client & server-side scripting, and network security configuration including use of multiple technologies. Web Development is an ever-evolving and an important skill that can lead to a successful career in the tech industry.",
     },
     {
         id: 6,
         image: DSGN,
-        name: "Designing",
-        content: "Designing refers to the process of creating a plan or blueprint for something, such as a product, system, or service. It involves identifying the needs and goals of the project, and creating a solution that meets those needs and goals while considering factors such as aesthetics, functionality, and cost.",
+        name: "UI/UX Designing",
+        content: "UI/UX design involves understanding the needs and goals of users and then designing visually pleasing and functional interfaces. UI/UX design is an important part of software development, as it allows for user feedback and testing that can be used to refine the design. It is a highly iterative process that focuses on creating an engaging and intuitive experience for users.",
     },
 ]
