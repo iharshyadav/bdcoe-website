65c450b7a7382f41761222f17cc0dce57bab347e
Prateek Chaurasia <prateek18801@gmail.com>
2023-01-29 01:32:14 +0530

    clean imports remove warnings

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-// import ComingSoon from './pages/ComingSoon';
 import Home from './pages/home'
 import Event from './pages/events'
 import EventReport from './pages/EventReport';
-//import Team from './pages(prev)/team'
 import Members from './pages/members'
 import './App.css';
-import {BrowserRouter as Router, Routes ,Route} from 'react-router-dom'
-import FacultyCards from './components/members/FacultyCards';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const App = () => {
-	return (
-		<>
-		<Router>
-			<Routes>
-				<Route exact path='/' element={<Home/>}/>		
-				<Route path='/team' element={<Members/>} />
-				<Route path='/event/:code' element={<EventReport/>} />
-				<Route path='/event' element={<Event/>} />		
-			</Routes>
-		</Router>
-		</>
-	);
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/team' element={<Members />} />
+          <Route path='/event/:code' element={<EventReport />} />
+          <Route path='/event' element={<Event />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
