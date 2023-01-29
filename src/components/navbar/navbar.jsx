736fdcb6abddf1f08add1353708cfecb5e135c98
Prateek Chaurasia <prateek18801@gmail.com>
2023-01-29 16:12:23 +0530

    add navbar register alert

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import logoWhite from '../../assets/logo-white1.svg'
 import './navbar.css';
 
@@ -11,34 +11,38 @@ function Navbar() {
     return navRef.current.classList.toggle("responsive_nav");
   }
 
+  function handleRegister() {
+    alert('More events coming soon!');
+  }
+
   return (
     <header>
       <nav ref={navRef}>
-        
-      <span className="logo" ><img src={logoWhite} alt="logo"/><span className="logo-name">BDCOE</span></span>
-        
+
+        <span className="logo" ><img src={logoWhite} alt="logo" /><span className="logo-name">BDCOE</span></span>
+
         <span className="links">
-          <a onClick={showNavbar} href="/#about">About</a>
-          <Link onClick={showNavbar} to="/event">Events</Link>
-          <a onClick={showNavbar} href="/#domains">Domains</a>
-          <Link onClick={showNavbar} to="/team">Team</Link>
-          <a onClick={showNavbar} href="/#contact">Contact Us</a>
+          <a onClick={showNavbar} href="/#about"><span>About</span></a>
+          <Link onClick={showNavbar} to="/event"><span>Events</span></Link>
+          <a onClick={showNavbar} href="/#domains"><span>Domains</span></a>
+          <Link onClick={showNavbar} to="/team"><span>Team</span></Link>
+          <a onClick={showNavbar} href="/#contact"><span>Contact Us</span></a>
         </span>
 
-        
-        <button className="regbtn">Register</button>
-        
+
+        <button className="regbtn" onClick={handleRegister}>Register</button>
+
         <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-        <FaTimes />
-          
+          <FaTimes />
+
         </button>
       </nav>
       <div className="resnav"><button className="nav-btn nav-open-btn" onClick={showNavbar}>
-      <FaBars />
+        <FaBars />
       </button>
       </div>
-      
-        
+
+
     </header>
   );
 }
