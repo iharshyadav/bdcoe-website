08b6596517ba803ae870541cb3517aa77bd078e0
Prateek Chaurasia <prateek18801@gmail.com>
2023-01-29 15:38:16 +0530

    fix faculty cards bug

--- src/pages/members.jsx
+++ src/pages/members.jsx
@@ -5,7 +5,7 @@ import './members.css'
 import FacultyCards from '../components/members/FacultyCards'
 import Navbar from '../components/navbar/navbar'
 const Members = () => {
-    const [batch, setBatch] = useState(2023);
+    const [batch, setBatch] = useState('2023');
     return <>
         <Navbar />
         <div className='members'>
@@ -13,13 +13,13 @@ const Members = () => {
                 <h1>Our Team</h1>
             </header>
             <select className='dropBatch' onChange={(e) => { setBatch(e.target.value) }} name="year">
-                <option value={2023}>4th Year</option>
-                <option value={2024}>3rd Year</option>
-                <option value={2025}>2nd Year</option>
-                <option value={2080}>Faculty</option>
-                <option value={1}>Alumni</option>
+                <option value={'2023'}>4th Year</option>
+                <option value={'2024'}>3rd Year</option>
+                <option value={'2025'}>2nd Year</option>
+                <option value={'2080'}>Faculty</option>
+                <option value={'1'}>Alumni</option>
             </select>
-            {batch === 2080 ? <FacultyCards /> : <Album batch={batch} />}
+            {batch === '2080' ? <FacultyCards /> : <Album batch={batch} />}
             <footer>
                 <h1>Creativity Inspires Our Implementation</h1>
                 <p>We at BDCOE are a passionate group of people who work towards bringing a change in the ecosystem of development around the campus. We want to create a healthy environment for the budding developers to exploring solutions to real life problems and promote the developer culture.</p>
