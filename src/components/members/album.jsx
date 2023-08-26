43cc71f09be0d3aa9d6570cd3c6a5a42eaedf574
nikhilsinha822 <113183657+nikhilsinha822@users.noreply.github.com>
2023-08-26 23:02:58 +0530

    team page loader added

--- src/components/members/album.jsx
+++ src/components/members/album.jsx
@@ -4,32 +4,51 @@ import axios from 'axios'
 import './album.css'
 const baseURL = "https://bdcoe.onrender.com/api/v1/member"
 const Album = ({ batch }) => {
-
     const [value, setValue] = useState()
+    const [isLoading, setisLoading] = useState(true);
     useEffect(() => {
-        axios.get(baseURL).then((response) => {
-            setValue(response.data);
-        });
+        (async () => {
+            try {
+                const response = await axios.get(baseURL)
+                setValue(response.data)
+            }
+            catch (err) {
+                console.log(`Error: ${err.message}`)
+            }
+            finally {
+                setisLoading(false)
+            }
+        })()
     }, []);
-
-    if (!value) return null;
-    if (value.imageUrl) return 11;
     return <>
-
         <div className='container-album'>
-            {value.data
-                .filter(prop =>
-                    String(prop.graduation < 2023 ? String(1) : String(prop.graduation)) === String(batch))
-                .map((i) =>
-                    <Card
-                        key={i._id}
-                        name={i.fullname}
-                        domain={i.domain}
-                        img={i.imageUrl}
-                        LI={i.linkedin}
-                        GH={i.github}
-                        IG={i.instagram}
-                    />)}
+            {
+                isLoading
+                    ?
+                    [...Array(12)].map((x, i) =>
+                        <div class="loader_card" key={i}>
+                            <div class="loader__image"></div>
+                            <div class="loader_content">
+                                <h2> </h2>
+                                <h2> </h2>
+                            </div>
+                        </div>
+                    )
+                    :
+                    value.data
+                        .filter(prop =>
+                            String(prop.graduation < 2024 ? String(1) : String(prop.graduation)) === String(batch))
+                        .map((i) =>
+                            <Card
+                                key={i._id}
+                                name={i.fullname}
+                                domain={i.domain}
+                                img={i.imageUrl}
+                                LI={i.linkedin}
+                                GH={i.github}
+                                IG={i.instagram}
+                            />)
+            }
         </div>
     </>
 }
